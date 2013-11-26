ecde639ffda40fc5b03eae9fa12f36ecdba62da7
KevinVillela <kevin@villela.com>
2013-11-26 10:54:42 -0500

    Added Some SQL Data
    I added a couple sandwiches from each restaurant to the mySQL database.

---
+++ Database/Data.sql
@@ -0,0 +1,17 @@
+insert into Restaurant values ("Subway");
+insert into Restaurant values ("Jimmy Johns");
+insert into Restaurant values ("Chipotle");
+insert into Restaurant values ("Qdoba");
+insert into Restaurant values ("Panera");
+
+insert into Meal values ("6 inch BLT",320, 9, 20, 650, "Subway");
+insert into Meal values ("6 inch Big Philly Cheesesteak",500, 17, 85, 1280, "Subway");
+insert into Meal values ("6 inch Cold Cut Combo",360, 12, 45, 1030, "Subway");
+insert into Meal values ("Pepe",580, 29, 55, 1380, "Jimmy Johns");
+insert into Meal values ("Turkey Tom",485, 20, 40, 1060, "Jimmy Johns");
+insert into Meal values ("Chicken White Rice Burrito",650, 19, 115, 1240, "Chipotle");
+insert into Meal values ("Burrito with Cheese",405, 14, 25, 1090, "Qdoba");
+insert into Meal values ("Plain Bagel",290, 1, 0, 460, "Panera");
+insert into Meal values ("Broccoli Cheddar Soup",330, 21, 75, 1390, "Panera");
+
+Select * from Meal;
--- Database/Tables.sql
+++ Database/Tables.sql
@@ -1,3 +1,5 @@
+drop table Restaurant;
+drop table Meal;
 CREATE TABLE Restaurant(
 	name VARCHAR(100) PRIMARY KEY
 );
@@ -5,18 +7,18 @@ CREATE TABLE Meal(
 	name VARCHAR(100) PRIMARY KEY,
 	calories INTEGER,
 	fat INTEGER,
-	saturated_fat INTEGER,
-	trans_fat INTEGER,
+	/*saturated_fat INTEGER,
+	trans_fat INTEGER,*/
 	cholesterol INTEGER,
 	sodium INTEGER,
-	carbohydrates INTEGER,
+	/*carbohydrates INTEGER,
 	fiber INTEGER,
 	sugar INTEGER,
 	protein INTEGER,
 	vitamin_a INTEGER,
 	vitamin_c INTEGER,
 	calcium INTEGER,
-	iron INTEGER,
+	iron INTEGER,*/
 	restaurant VARCHAR(100) NOT NULL,
 	foreign key fk_1 (restaurant) references Restaurant(name)
 );
